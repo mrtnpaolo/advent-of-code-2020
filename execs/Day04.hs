6c7642730624fdce4900f8bcc0d6db756e683909
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-04 23:01:41 +0100

    🛠 day-04: rewrite for video

--- execs/Day04.hs
+++ execs/Day04.hs
@@ -3,49 +3,60 @@ module Main
   ) where
 
 import Advent
-import Data.List.Split (splitOn)
 import Data.Char (isDigit)
+import Data.List.Split (splitOn)
 
 type Passport = [(String,String)]
 
 main :: IO ()
 main =
-  do x <- parse <$> getRawInput 4
-     print (part1 x)
-     print (part2 x)
+  do ps <- parse <$> getRawInput 4
+     print (part1 ps)
+     print (part2 ps)
   where
-    parse = map (map parsePair . words) . splitOn "\n\n"
-    parsePair (a:b:c:':':value) = (a:b:c:[],value)
-    parsePair _ = undefined
+    parse :: String -> [Passport]
+    parse = map parsePassport . splitOn "\n\n"
 
-part1 :: [Passport] -> Int
-part1 = count valid1
+    parsePassport :: String -> Passport
+    parsePassport = map parseKey . words
 
-valid1 :: Passport -> Bool
-valid1 passport = all (`elem` (map fst passport)) expected
+    parseKey :: String -> (String,String)
+    parseKey (a:b:c:':':v) = (a:b:c:[],v)
+    parseKey _             = undefined
+
+part1, part2 :: [Passport] -> Int
+part1 = count valid
+
+valid :: Passport -> Bool
+valid p = all (`elem` (map fst p)) keys
   where
-    expected = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
+    keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
-part2 :: [Passport] -> Int
-part2 = count valid2 . filter valid1
+part2 = count (all (uncurry strict)) . filter valid
 
-valid2 :: Passport -> Bool
-valid2 passport = all (uncurry validVal) passport
+strict :: String -> String -> Bool
+
+strict "byr" (ri -> n) = 1920 <= n && n <= 2002
+strict "iyr" (ri -> n) = 2010 <= n && n <= 2020
+strict "eyr" (ri -> n) = 2020 <= n && n <= 2030
+
+strict "hgt" (reverse -> xs)
+  | ('m':'c':(ri . reverse -> n)) <- xs = 150 <= n && n <= 193
+  | ('n':'i':(ri . reverse -> n)) <- xs =  59 <= n && n <=  76
+  | otherwise = False
+
+strict "hcl" ('#':xs) =
+  length xs == 6 && all (`elem` "0123456789abcdef") xs
+
+strict "ecl" xs =
+  xs `elem` ["amb","blu","brn","gry","grn","hzl","oth"]
+
+strict "pid" xs =
+  length xs == 9 && all isDigit xs
+
+strict "cid" _ = True
+
+strict _ _ = False
 
 ri :: String -> Int
 ri = read
-
-validVal :: String -> String -> Bool
-validVal "byr" (ri -> n) = 1920 <= n && n <= 2002
-validVal "iyr" (ri -> n) = 2010 <= n && n <= 2020
-validVal "eyr" (ri -> n) = 2020 <= n && n <= 2030
-validVal "hgt" (reverse -> v)
-  | ('n':'i':(ri . reverse -> h)) <- v =  59 <= h && h <=  76
-  | ('m':'c':(ri . reverse -> h)) <- v = 150 <= h && h <= 193
-  | otherwise                          = False
-validVal "hcl" ('#':xs) = length xs == 6 && all (`elem` "0123456789abcdef") xs
-validVal "hcl" _        = False
-validVal "ecl" ecl = ecl `elem` ["amb","blu","brn","gry","grn","hzl","oth"]
-validVal "pid" pid = length pid == 9 && all isDigit pid
-validVal "cid" _   = True
-validVal _     _   = False
