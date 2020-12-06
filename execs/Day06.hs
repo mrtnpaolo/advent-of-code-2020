ea21ca4e517a683938ea93a5eb60441997380412
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-06 08:21:43 +0100

    🛠 day-06: simplify a little bit

--- execs/Day06.hs
+++ execs/Day06.hs
@@ -3,7 +3,7 @@ module Main
   ) where
 
 import Advent
-import Data.List (sort,nub,foldl1',intersect)
+import Data.List (union,intersect)
 import Data.List.Split (splitOn)
 
 main :: IO ()
@@ -11,12 +11,9 @@ main =
   do i <- getParsed parse 6
      print (part1 i)
      print (part2 i)
-
   where
     parse = map words . splitOn "\n\n"
 
 part1, part2 :: [[String]] -> Int
-
-part1 = sum . map (length . nub . sort . concat)
-
-part2 = sum . map (length . nub . foldl1' intersect)
+part1 = sum . map (length . foldr union [])
+part2 = sum . map (length . foldl1 intersect)
