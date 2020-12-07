f6bd6d88c440bae190bfe6b5a43f6c1893dc6dec
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-07 12:38:40 +0100

    🛠 day-07: part 1 more tidy

--- execs/Day07.hs
+++ execs/Day07.hs
@@ -1,6 +1,4 @@
-{-# OPTIONS_GHC -Wno-missing-signatures #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 module Main
   ( main
   ) where
@@ -9,39 +7,28 @@ import Advent
 import qualified Data.Map.Strict as M
 import Data.List (unfoldr,partition)
 import Data.List.Split (splitOn)
+import Data.Bifunctor (bimap)
+
+type Rule = (String,[(Int,String)])
 
 main :: IO ()
 main =
   do rs <- getParsedLines parseLine 7
      print (part1 rs)
 
+parseLine :: String -> Rule
 parseLine = match . words . init
   where
-    match (c1:c2:"bags":"contain":(concatMap (each . words) . splitOn ", " . unwords -> bags)) =
-      (unwords [c1,c2],bags)
+    match (c1:c2:"bags":"contain":inside) = (unwords [c1,c2],bags)
+      where
+        bags = concatMap (each . words) . splitOn ", " . unwords $ inside
     each ["no","other","bags"]      = []
     each ((read @Int -> n):c1:c2:_) = [(n,unwords [c1,c2])]
 
 -- open each bag until either shiny gold or no bags are inside
-part1 rs = sum $ takeWhile (>0) $ unfoldr open bags
+part1 :: [Rule] -> Int
+part1 rules = sum $ takeWhile (>0) $ unfoldr (Just . open) (M.elems contain)
   where
-    rs' = M.fromList [ (c,cs) | (c,map snd -> cs) <- rs ]
-    bags = [ cs | (_,map snd -> cs) <- rs ]
-    open xs =
-      case partition ("shiny gold" `elem`) xs of
-        (length -> n,rest) -> Just (n,map (concatMap (rs' M.!)) rest)
-
-{-
--- invert the map
--- then start from "shiny gold" keeping track of all the colors seen
-part1 rs = go S.empty (S.singleton "shiny gold")
-  where
-    inverted = M.fromListWith (++) [ y | (c,map snd -> cs) <- rs
-                                       , y <- (\x -> (x,[c])) <$> cs ]
-    go seen cs
-      | S.null cs = S.size seen
-      | otherwise = go seen' cs'
-      where
-        cs' = S.fromList . concat . M.elems . M.restrictKeys inverted $ cs
-        seen' = seen `S.union` cs'
--}
+    open = bimap length (map openAll) . partition ("shiny gold" `elem`)
+    openAll = concatMap (contain M.!)
+    contain = M.fromList [ (c,map snd cs) | (c,cs) <- rules ]
