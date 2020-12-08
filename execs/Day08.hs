976f306ce658d3d0237c29fec643257e0f807ab1
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-08 07:32:57 +0100

    🛠 day-08: tidy up

--- execs/Day08.hs
+++ execs/Day08.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns -Wno-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Main
   ( main
   ) where
@@ -13,58 +13,70 @@ import Data.List (findIndices)
 main :: IO ()
 main =
   do i <- getParsedLines parse 8
-     --i <- map parse . lines <$> getRawTest 8 1
+     -- i <- map parse . lines <$> getRawTest 8 1
      print (part1 i)
-     part2 i
+     print `mapM_` part2 i
 
 type Ins = (String,Int)
 
 parse :: String -> Ins
-parse (words -> [ins,'+':(read -> off)]) = (ins, off)
-parse (words -> [ins,'-':(read -> off)]) = (ins,-off)
+parse (words -> [op,'+':(read -> arg)]) = (op, arg)
+parse (words -> [op,'-':(read -> arg)]) = (op,-arg)
 
-part1 = _acc . run . load
+part1 :: [Ins] -> Int
+part1 = finalAcc . run (-1) . load
 
 data VM = VM
-  { _mem :: IntMap Ins
+  { _mem  :: IntMap Ins
   , _seen :: Set Int
-  , _acc :: Int
-  , _ip :: Int
+  , _acc  :: Int
+  , _ip   :: Int
   } deriving (Show)
 
+load :: [Ins] -> VM
 load xs =
-  VM { _mem = IM.fromList (zip [0..] xs)
+  VM { _mem  = IM.fromList (zip [0..] xs)
      , _seen = S.empty
-     , _acc = 0
-     , _ip = 0
+     , _acc  = 0
+     , _ip   = 0
      }
 
-run vm@VM{..}
-  | S.member _ip _seen = vm
-  | _ip == IM.size _mem = vm
-  | otherwise =
-    case _mem IM.! _ip of
-      ("acc",n)   -> run $ vm { _acc = _acc + n
-                              , _ip = _ip + 1
-                              , _seen = S.insert (_ip) _seen
-                              }
-      ("jmp",off) -> run $ vm { _ip = _ip + off
-                              , _seen = S.insert (_ip) _seen
-                              }
-      ("nop",_)   -> run $ vm { _ip = _ip + 1
-                              , _seen = S.insert (_ip) _seen
-                              }
+data Effect = Infinite Int | Terminate Int
 
--- 232 jmps, 73 nops, 305 total
-part2 mem = print `mapM_` (map _acc successes)
+finalAcc :: Effect -> Int
+finalAcc (Infinite  acc) = acc
+finalAcc (Terminate acc) = acc
+
+run :: Int {- ^ index of instruction to flip -} -> VM -> Effect
+run ix vm@VM{..}
+
+  -- infinite loop
+  | S.member _ip _seen = Infinite _acc
+
+  -- correct termination
+  | _ip == IM.size _mem = Terminate _acc
+
+  -- instruction processing
+  | otherwise = run ix $ case instr of
+
+      ("jmp",off) -> ip off .         look $ vm
+      ("nop",_  ) -> ip 1   .         look $ vm
+      ("acc",n  ) -> ip 1   . acc n . look $ vm
+
+    where
+      patch "jmp" = "nop"
+      patch "nop" = "jmp"
+      patch   x   =   x
+
+      instr = case _mem IM.! _ip of
+        (op,arg) | _ip == ix -> (patch op,arg)
+                 | otherwise -> (      op,arg)
+
+      look   vm@VM{..} = vm { _seen = S.insert _ip _seen }
+      ip off vm@VM{..} = vm { _ip   = _ip + off }
+      acc n  vm@VM{..} = vm { _acc  = _acc + n }
+
+part2 :: [Ins] -> [Int]
+part2 mem = [ n | Terminate n <- [ run i (load mem) | i <- indices ] ]
   where
-    jis = findIndices (("jmp"==).fst) mem
-    nis = findIndices (("nop"==).fst) mem
-    alts =
-      [ patch "nop" i | i <- jis ] ++
-      [ patch "jmp" i | i <- nis ]
-    patch replacement index = rhs ++ (replacement,n) : lhs
-      where
-        (rhs,(_,n):lhs) = splitAt index mem
-    successes =
-      filter (\VM{..} -> _ip == IM.size _mem) (map (run . load) alts)
+    indices = findIndices ((`elem` ["jmp","nop"]) . fst) mem
