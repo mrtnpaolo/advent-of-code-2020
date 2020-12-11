bf8ab3394411bc6fb192b74440b67eb649809c68
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-11 07:01:49 +0100

    🧼 day-11: tidy up

--- execs/Day11.hs
+++ execs/Day11.hs
@@ -1,29 +1,23 @@
-{-# OPTIONS_GHC -Wno-missing-signatures -Wno-unused-imports #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Main
   ( main
   ) where
 
 import Advent
 import Advent.Coord
-import Advent.Search
-import Data.Maybe
-import Data.List
-import Data.List.Split
-import qualified Data.Set as S
-import qualified Data.IntSet as IS
+import Data.Maybe (maybeToList,listToMaybe,catMaybes,isJust)
 import qualified Data.Map.Strict as M
-import qualified Data.IntMap.Strict as IM
-import Debug.Trace
 
 main :: IO ()
 main =
   do grid <- getParsed parse 11
-     --grid <- getParsedTest parse 11 1
+     print (part1 grid)
      print (part2 grid)
 
 data Seat = Floor | Empty | Occupied
   deriving (Show,Eq)
 
+parse :: String -> Grid
 parse i = M.fromList $ concat $
   [ [ (C y x,the thing) | (x,thing) <- zip [0..] xs ]
   | (y,xs) <- zip [0..] (lines i) ]
@@ -33,25 +27,29 @@ parse i = M.fromList $ concat $
     the '#' = Occupied
     the  _  = undefined
 
+part1 :: Grid -> Int
 part1 = M.size . M.filter (== Occupied) . dup . iterate tick
 
 type Grid = M.Map Coord Seat
 
-adj g c =
-  [ seat | c' <- neighbors c, seat <- maybeToList (g M.!? c') ]
+adj :: Grid -> Coord -> [Seat]
+adj g c = [ seat | c' <- neighbors c, seat <- maybeToList (g M.!? c') ]
 
 tick :: Grid -> Grid
 tick g = M.mapWithKey f g
   where
-    f c Empty    | all (/= Occupied) (adj g c) = Occupied
+    f c Empty    | all   (/= Occupied) (adj g c)      = Occupied
     f c Occupied | count (== Occupied) (adj g c) >= 4 = Empty
     f _ seat = seat
 
+dup :: Eq a => [a] -> a
 dup (x:y:_) | x == y = x
-dup (_:xs) = dup xs
+dup (_:xs)           = dup xs
 
+part2 :: Grid -> Int
 part2 = M.size . M.filter (== Occupied) . dup . iterate tick2
 
+far :: Grid -> Coord -> [Seat]
 far g (C y x) = concatMap look' dirs
   where
     ray (C dy dx) = [ g M.!? C (y+i) (x+j)
