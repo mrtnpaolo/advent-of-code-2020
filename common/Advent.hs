66c9f74b03bdc99374ebc1233cdb08f77110316d
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-14 18:27:13 +0100

    ⏳ day-01: off time cleanup

--- common/Advent.hs
+++ common/Advent.hs
@@ -7,32 +7,29 @@ import System.Environment (getArgs)
 import Text.Printf (printf)
 import Data.List (foldl')
 
-getRawInput :: Int {- ^ day number -} -> IO String
-getRawInput n =
+getInput :: (String -> a) -> Int {- ^ day number -} -> IO a
+getInput parse n =
   do args <- getArgs
-     case args of
+     parse <$> case args of
        []    -> readFile (printf "inputs/input-%02d.txt" n)
        "-":_ -> getContents
        fn:_  -> readFile fn
 
-getParsed :: (String -> a) -> Int -> IO a
-getParsed f n = f <$> getRawInput n
-
-getParsedLines :: (String -> a) -> Int -> IO [a]
-getParsedLines f n = map f . lines <$> getRawInput n
-
-getRawTest :: Int {- ^ day number -} -> Int {- ^ test number -} -> IO String
-getRawTest n m =
+getTest :: (String -> a)
+        -> Int {- ^ day number -}
+        -> Int {- ^ test number -}
+        -> IO a
+getTest parse n m =
   do args <- getArgs
-     case args of
+     parse <$> case args of
        []    -> readFile (printf "inputs/input-%02d-test-%02d.txt" n m)
        "-":_ -> getContents
 
-getParsedTest :: (String -> a) -> Int -> Int -> IO a
-getParsedTest f n i = f <$> getRawTest n i
+getInputLines :: (String -> a) -> Int -> IO [a]
+getInputLines parse = getInput (map parse . lines)
 
-getParsedTestLines :: (String -> a) -> Int -> Int -> IO [a]
-getParsedTestLines f n i = map f . lines <$> getRawTest n i
+getTestLines :: (String -> a) -> Int -> Int -> IO [a]
+getTestLines parse = getTest (map parse . lines)
 
 count :: Foldable f => (a -> Bool) -> f a -> Int
 count p = foldl' (\n x -> if p x then n+1 else n) 0
--- execs/Day01.hs
+++ execs/Day01.hs
@@ -2,15 +2,19 @@ module Main
   ( main
   ) where
 
-import Advent
+import Advent (getInputLines)
 import Data.List (tails)
 
 main :: IO ()
 main =
-  do xs <- map (read @Int) . lines <$> {- getRawTest 1 1 -} getRawInput 1
-     print (part1 xs)
-     print (part2 xs)
+  do i <- getInputLines parse 1
+     print (part1 i)
+     print (part2 i)
 
-part1, part2 :: [Int] -> Int
-part1 xs = head [ x*y | x:ys <- tails xs, y <- ys, x+y == 2020 ]
-part2 xs = head [ x*y*z | x:ys <- tails xs, y:zs <- tails ys, z <- zs, x+y+z == 2020 ]
+parse = read @Int
+
+part1 xs =
+  head [ x*y   | x:ys <- tails xs, y <- ys, x+y == 2020 ]
+
+part2 xs =
+  head [ x*y*z | x:ys <- tails xs, y:zs <- tails ys, z <- zs, x+y+z == 2020 ]
