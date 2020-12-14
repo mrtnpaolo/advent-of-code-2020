fe7c822d3584a94a26a47557518cc260fed0b4d1
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-14 21:30:42 +0100

    🧼 day-06: off time cleanup

--- execs/Day06.hs
+++ execs/Day06.hs
@@ -2,18 +2,18 @@ module Main
   ( main
   ) where
 
-import Advent
+import Advent (getInput)
 import Data.List (union,intersect)
 import Data.List.Split (splitOn)
 
 main :: IO ()
 main =
-  do i <- getParsed parse 6
+  do i <- getInput parse 6
      print (part1 i)
      print (part2 i)
-  where
-    parse = map words . splitOn "\n\n"
 
-part1, part2 :: [[String]] -> Int
+parse = map words . splitOn "\n\n"
+
 part1 = sum . map (length . foldr union [])
+
 part2 = sum . map (length . foldl1 intersect)
