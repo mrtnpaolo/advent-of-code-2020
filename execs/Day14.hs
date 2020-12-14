c177958a2aff1f713b08ab3c566be8bbca1c0b09
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-14 23:52:25 +0100

    🧼 day-14: off time cleanup

--- execs/Day14.hs
+++ execs/Day14.hs
@@ -1,9 +1,8 @@
-{-# OPTIONS_GHC -Wno-missing-signatures #-}
 module Main
   ( main
   ) where
 
-import Advent
+import Advent (getInputLines)
 
 import Data.Bits (setBit,clearBit,(.&.),(.|.))
 import Data.List (foldl')
@@ -14,14 +13,13 @@ import Control.Monad (filterM)
 
 main :: IO ()
 main =
-  do i <- getParsedLines parse 14
+  do i <- getInputLines parse 14
      print (part1 i)
      print (part2 i)
 
 data Ins
   = Set !Int !Int
-  | Mask !Int {- zeros: and -} !Int {- ones: or -} [Int] {- floating -}
-  deriving (Show)
+  | Mask !Int {- zeros -} !Int {- ones -} [Int] {- floating -}
 
 parse = match . words . map r
   where
@@ -34,30 +32,32 @@ parse = match . words . map r
     ones     mask = foldl' setBit 0 [ i | (i,'1') <- zip [0..] mask ]
     floating mask = [ i | (i,'X') <- zip [0..] mask ]
 
-noMask = Mask (foldl' setBit 0 [0..36]) (0) []
+noMask = Mask (foldl' setBit 0 [0..36]) (0) ([])
 
 part1 = go IM.empty noMask
   where
-    go mem _ [] = sum mem
-
-    go mem m@(Mask z o _) (Set addr n : rest) = go mem' m rest
+    go mem m (Set addr n : rest) = go mem' m rest
       where
-        mem' = IM.insert addr n' mem
-        n'   = (n .&. z) .|. o
+        Mask z o _ = m
+        mem'       = IM.insert addr n' mem
+        n'         = (n .&. z) .|. o
 
     go mem _ (m : rest) = go mem m rest
+
+    go mem _ [] = sum mem
 
 part2 = go IM.empty noMask
   where
-    go mem _ [] = sum mem
-
-    go mem m@(Mask _ o f) (Set addr n : rest) = go mem' m rest
+    go mem m (Set addr n : rest) = go mem' m rest
       where
-        mem'   = foldl' insert mem addrs
-        insert = \prev at -> IM.insert at n prev
-        addrs  = [ foldl' setBit addr' js | js <- subsets f ]
-        addr'  = foldl' clearBit (addr .|. o) f
+        Mask _ o f = m
+        mem'       = foldl' insert mem addrs
+        insert     = \prev at -> IM.insert at n prev
+        addrs      = [ foldl' setBit addr' js | js <- subsets f ]
+        addr'      = foldl' clearBit (addr .|. o) f
 
     go mem _ (m : rest) = go mem m rest
+
+    go mem _ [] = sum mem
 
 subsets = filterM (const [True,False])
