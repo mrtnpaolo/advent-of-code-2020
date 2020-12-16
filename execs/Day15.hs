d53ab585e1d2a83f420cba108a679a21d8fbbb43
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-16 19:46:21 +0100

    🏎 day-15: faster solution

--- execs/Day15.hs
+++ execs/Day15.hs
@@ -2,36 +2,34 @@ module Main
   ( main
   ) where
 
-import Advent (getInput)
-
+import Advent          (getInput)
+import Data.Int        (Int32)
 import Data.List.Split (splitOn)
+import Control.Monad   (zipWithM_,when,unless)
+import Text.Printf     (printf)
+import Debug.Trace     (traceM)
 
-import qualified Data.IntMap.Strict as IM
+import Control.Monad.ST  qualified as ST (ST)
+import Data.Array.ST     qualified as ST (STUArray,runSTUArray)
+import Data.Array.MArray qualified as A  (newArray,writeArray,readArray)
 
 main :: IO ()
 main =
-  do i <- getInput parse 15
-     print (part1 i)
-     print (part2 i)
+  do start <- getInput (map (read @Int32) . splitOn ",") 15
+     ST.runSTUArray (play start) `seq` pure ()
 
-parse = map (read @Int) . splitOn ","
+play :: [Int32] -> ST.ST s (ST.STUArray s Int32 Int32)
+play start =
+  do arr <- A.newArray (0,30_000_000) (0)
+     zipWithM_ (A.writeArray arr) (init start) [1..]
+     loop arr (1 + fromIntegral (length start)) (last start)
+     return arr
 
-part1 = (!! (2020 - 1)) . play
-
-part2 = (!! (30000000 - 1)) . play
-
-play ns = game
-  where
-    game = ns ++ go (IM.fromList $ zip ns [ [i] | i <- [1..] ]) (1 + length ns) (last ns)
-
-    go !seen !i !lastnumber
-      = n `seq` n : go (IM.insertWith upd n [i] seen) (i+1) n
-        where
-          n = case seen IM.!? lastnumber of
-                Just turns | length turns > 1 -> let (t1:t0:_) = turns in t1-t0
-                _ -> 0
-
--- upd new old
-upd [new] [] = [new]
-upd [new] [t] = [new,t]
-upd [new] (t:_) = [new,t]
+loop arr i last_n =
+  do t0 <- A.readArray arr last_n
+     let t1 = i-1
+     let n | t0 == 0 = 0 | otherwise = t1-t0
+     A.writeArray arr last_n t1
+     when   (2020 == i)     $ traceM (printf "2020: %d" n)
+     when   (30_000_000 == i) $ traceM (printf "30000000: %d" n)
+     unless (30_000_000 == i) $ loop arr (i+1) n
