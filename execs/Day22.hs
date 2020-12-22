f909e399f75b3fb08a2996525a84813ceab63e40
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-22 07:40:44 +0100

    🧼 day-22: tidy up

--- execs/Day22.hs
+++ execs/Day22.hs
@@ -1,37 +1,15 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Main
   ( main
   ) where
 
-import Advent
+import Advent          (getInput)
+import Data.List.Split (splitOn)
 
-import Data.Ix
-import Data.Ord
-import Data.List
-import Data.Bits
-import Data.Char
-import Data.Maybe
-import Data.Foldable
-import Data.Traversable
-
-import Control.Monad
-import Control.Applicative
-
-import Data.Sequence
-import Data.Sequence      qualified as  Seq
-import Data.Set           qualified as  S
-import Data.IntSet        qualified as IS
-import Data.Map.Strict    qualified as  M
-import Data.IntMap.Strict qualified as IM
-
-import Data.Array.IArray  qualified as  A
-
-import Data.List.Split
-
-import Debug.Trace
-
---import Text.Regex.Base
---import Text.Regex.TDFA
+import Data.Foldable                    (toList)
+import Data.Sequence                    (Seq(..),(|>))
+import Data.Sequence   qualified as Seq (fromList,null,length,take)
+import Data.Set        qualified as Set (empty,member,insert)
 
 main :: IO ()
 main =
@@ -43,54 +21,37 @@ parse i = (Seq.fromList p1, Seq.fromList p2)
   where
     [p1,p2] = map (map (read @Int)) . map (tail . lines) . splitOn "\n\n" $ i
 
-part1 = score . winner . head . dropWhile noWinner . {- Data.List.take 30 . -} iterate tick
+part1 = score . winner . until finished combat
 
-tick (p1,p2) = (p1',p2')
-  where
-    (x :<| xs) = p1
-    (y :<| ys) = p2
-    p1' | x > y = xs |> x |> y
-        | True  = xs
-    p2' | y > x = ys |> y |> x
-        | True  = ys
+combat (x :<| xs,y :<| ys)
+  | x > y      = (xs |> x |> y,ys)
+  | otherwise  = (xs,ys |> y |> x)
 
-noWinner (_ :<| _,_ :<| _) = True
-noWinner _ = False
+finished (p1,p2) = Seq.null p1 || Seq.null p2
 
-winner (Empty,p2) = p2
 winner (p1,Empty) = p1
+winner (Empty,p2) = p2
 
-score = sum . Data.List.zipWith (*) [1..] . Data.List.reverse . toList
+score = sum . zipWith (*) [1..] . reverse . toList
 
-part2 ps = score . winner . head . dropWhile noWinner . map snd . iterate ({-traceShowId . -}rtick) $ (S.empty,{-traceShowId -}ps)
+part2 = score . winner . snd . until (finished . snd) rcombat . (Set.empty,)
 
-rtick (history,players@(p1,p2))
-  | players `S.member` history = (history,(p1,Empty))
-  | otherwise =
-    if Seq.length xs >= x && Seq.length ys >= y
-      then let p1' = Seq.take x xs
-               p2' = Seq.take y ys
-               ps' = (p1',p2')
-               result = head . dropWhile noWinner . map snd . iterate rtick $ (history,ps')
-           in {-traceShow ("subgame",ps') $-} case result of
-                (_,Empty) -> (history',(xs |> x |> y,ys))
-                (Empty,_) -> (history',(xs,ys |> y |> x))
-      else if x > y then (history',(xs |> x |> y,ys))
-                    else (history',(xs,ys |> y |> x))
-    where
-      (x :<| xs) = p1
-      (y :<| ys) = p2
-      history' = S.insert players history
+rcombat (seen,players)
 
-opptick (p1,p2) = (p1',p2')
+  -- repeating
+  | players `Set.member` seen = (seen,(fst players,Empty))
+
+  -- can play a subgame
+  | Seq.length xs >= x && Seq.length ys >= y = case recursive of
+      (_,Empty) -> (seen',(xs |> x |> y,ys))
+      (Empty,_) -> (seen',(xs,ys |> y |> x))
+
+  -- or the bigger draw wins
+  | x > y = (seen',(xs |> x |> y,ys))
+  | True  = (seen',(xs,ys |> y |> x))
+
   where
-    (x :<| xs) = p1
-    (y :<| ys) = p2
-    p1' | x < y = xs |> x |> y
-        | True  = xs
-    p2' | y < x = ys |> y |> x
-        | True  = ys
-
-score2 (p1,_) (_,Empty) = score p1
-score2 (_,p2) (Empty,_) = score p2
-
+    (x :<| xs,y :<| ys) = players
+    seen' = Set.insert players seen
+    recursive = snd $
+      until (finished . snd) rcombat (seen,(Seq.take x xs,Seq.take y ys))
