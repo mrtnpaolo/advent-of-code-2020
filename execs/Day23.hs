9a90213d16b87700532f717d0c3a8523e78abc4e
Paolo Martini <mrtnpaolo@protonmail.com>
2020-12-23 14:32:44 +0100

    🧼 day-23: tidy up part 2

--- execs/Day23.hs
+++ execs/Day23.hs
@@ -1,48 +1,23 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main
   ( main
   ) where
 
-import Advent
-
-import Data.Ix
-import Data.Ord
-import Data.List
-import Data.Bits
-import Data.Char
-import Data.Maybe
-import Data.Foldable
-import Data.Traversable
-
-import Control.Monad
-import Control.Applicative
+import Data.List (sort)
+import Data.Foldable (toList)
 
 import Data.Sequence (Seq(..),(><),(<|))
-import Data.Sequence      qualified as Seq
-import Data.Set           qualified as  S
-import Data.IntSet        qualified as IS
-import Data.Map.Strict    qualified as  M
-import Data.IntMap.Strict qualified as IM
+import Data.Sequence qualified as Seq
 
-import Data.Array.IArray  qualified as  A
-
-import Data.List.Split
-
-import Debug.Trace
-
---import Text.Regex.Base
---import Text.Regex.TDFA
-
-import Control.Monad.ST qualified as ST
-import Data.Array.ST qualified as ST
-import Data.Array.Unboxed qualified as U
-import Data.Array.MArray qualified as M
-import Data.Array.IArray qualified as A
+import Control.Monad.ST   (ST)
+import Data.Array.ST      qualified as ST (runSTUArray,STUArray)
+import Data.Array.Unboxed qualified as U (UArray)
+import Data.Array.MArray  qualified as M (newListArray,readArray,writeArray)
+import Data.Array.IArray  qualified as A ((!))
 
 main :: IO ()
 main =
-  do print (part1 9 start)
+  do print (read @Int $ concatMap show $ toList $ part1 9 start)
      print (part2 1_000_000 (toList start))
 
 type Cups = Seq Int
@@ -58,9 +33,7 @@ part1 m = f . (!! 100) . iterate (tick m)
 
 tick m (curr :<| a :<| b :<| c :<| rest) = rotated
   where
-    picked =  S.fromList [a,b,c]
     remain = curr :<| rest
-    --[next] = take 1 . filter (`S.notMember` picked) . tail . iterate (pred' m) $ curr
     next = until (\n -> n/=a&&n/=b&&n/=c) (pred' m) (pred' m curr)
     (left,_ :<| right) = Seq.spanl (next /=) remain
     new = left >< (next <| a <| b <| c <| right)
@@ -73,12 +46,10 @@ f s = r <> l
   where
     (l,1 :<| r) = Seq.spanl (1 /=) s
 
---part2 :: Int -> [Int] -> Int
 part2 m s = (a A.! 1) * (a A.! (a A.! 1))
--- zipWith (A.!) (repeat a) ([1..12] ++ [999_997,999_998,999_999,1_000_000])
   where
     a :: U.UArray Int Int = ST.runSTUArray $
-          do arr <- M.newListArray (1,m) (map snd cups) :: ST.ST s (ST.STUArray s Int Int)
+          do arr <- M.newListArray (1,m) (map snd cups) :: ST s (ST.STUArray s Int Int)
              go 1 arr (head s)
     (lastcup,firstcup) : (reverse -> restcups) = reverse $ zip s (tail (cycle s))
     extra = succ (maximum s)
@@ -86,9 +57,8 @@ part2 m s = (a A.! 1) * (a A.! (a A.! 1))
              zip [extra .. pred m] (map succ [extra .. pred m]) ++
                [(m,firstcup)]
 
-    go :: forall s. Int -> ST.STUArray s Int Int -> Int -> ST.ST s (ST.STUArray s Int Int)
-    go 10_000_000 arr _ = pure arr :: ST.ST s (ST.STUArray s Int Int)
-    --go 10 arr _ = pure arr :: ST.ST s (ST.STUArray s Int Int)
+    go :: forall s. Int -> ST.STUArray s Int Int -> Int -> ST s (ST.STUArray s Int Int)
+    go 10_000_000 arr _ = pure arr :: ST s (ST.STUArray s Int Int)
     go i arr curr =
       do -- pick up cups a,b,c and tie curr to d to re-make the circle
          -- curr,a,b,c,d -> curr,d
@@ -99,12 +69,10 @@ part2 m s = (a A.! 1) * (a A.! (a A.! 1))
          M.writeArray arr curr d
          -- calc the destination
          let dest = until (\n -> n/=a&&n/=b&&n/=c) (pred' m) (pred' m curr)
-         -- find dest,x and plop a,b,c into them: dest,a,b,c,x
+         -- find dest,x and plop a,b,c between them: dest,a,b,c,x
          x <- M.readArray arr dest
          M.writeArray arr dest a
          M.writeArray arr c x
          -- find the next current cup
          next <- M.readArray arr curr
-         when (i `mod` 1_000_000 == 0) $
-            traceM (show (i,curr,next))
          go (succ i) arr next
